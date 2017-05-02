80806dca92dd11d2a8b72b159656c348da1415bc
Jerad Rutnam <jeradrutnam@outlook.com>
2017-05-02 12:00:39 +0530

    Added SASS files

--- modules/web/index.js
+++ modules/web/index.js
@@ -3,11 +3,10 @@ window.$ = window.jQuery = require("jquery");
 window.simulate = window.jQuerySimulate = require("./lib/jquery-simulate-1.0.0/jquery.simulate.js");
 window.scope = window.location.pathname;
 
-
 var Application = require('./js/main').default;
-var ApplicationStyles = require('./scss/main.scss').default;
 const config = require('./config').default;
 var app = new Application(config);
+var appTheme = require('./scss/themes/light.scss').default;
 
 app.render();
 app.displayInitialView();
