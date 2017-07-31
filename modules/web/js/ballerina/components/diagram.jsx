73965a4f5151d484e4946f015bbc10575d4dbe37
manuranga <manu@wso2.com>
2017-07-31 13:38:01 +0530

    Moving visitors to ballerina-file-editor so js error can block diagram.

--- modules/web/js/ballerina/components/diagram.jsx
+++ modules/web/js/ballerina/components/diagram.jsx
@@ -20,8 +20,6 @@ import React from 'react';
 import log from 'log';
 import PropTypes from 'prop-types';
 import CanvasDecorator from './canvas-decorator';
-import PositionCalcVisitor from '../visitors/position-calculator-visitor';
-import DimensionCalcVisitor from '../visitors/dimension-calculator-visitor';
 import AnnotationRenderingVisitor from '../visitors/annotation-rendering-visitor';
 import { getComponentForNodeArray } from './utils';
 import BallerinaASTRoot from './../ast/ballerina-ast-root';
@@ -44,8 +42,6 @@ class Diagram extends React.Component {
     constructor(props) {
         super(props);
         this.sourceGen = new SourceGenVisitor();
-        this.dimentionCalc = new DimensionCalcVisitor();
-        this.positionCalc = new PositionCalcVisitor();
     }
 
     /**
@@ -70,22 +66,7 @@ class Diagram extends React.Component {
 
     visitModel(nextProps) {
         try {
-            if (nextProps.show) {
-                // Following is how we render the diagram.
-                // 1. We will visit the model tree and calculate width and height of all
-                //    the elements. We will use DimensionCalcVisitor.
-                nextProps.model.accept(this.dimentionCalc);
-                // 1.5 We need to adjest the width of the panel to accomodate width of the screen.
-                // - This is done by passing the container width to position calculater to readjest.
-                const viewState = nextProps.model.getViewState();
-                viewState.container = {
-                    width: $(this.context.getDiagramContainer()).width(),
-                    height: $(this.context.getDiagramContainer()).height(),
-                };
-                // 2. Now we will visit the model again and calculate position of each node
-                //    in the tree. We will use PositionCalcVisitor for this.
-                nextProps.model.accept(this.positionCalc);
-            }
+            // 1 , 1.5 and 2 steps has been moved up to ballerina-file-editor.
             // 3. Now we need to create component for each child of root node.
             let [others] = [undefined, [], [], []];
             const otherNodes = [];
@@ -136,7 +117,7 @@ class Diagram extends React.Component {
                 {this.state.others}
             </CanvasDecorator>);
         } else {
-            return <div>error</div>;
+            return <div>Diagram error.</div>;
         }
     }
 }
